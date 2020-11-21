06e3b016a8bd3a43c8e13ed6a958ce8b8e5549b4
AntonJ <anton.javits@gmail.com>
2020-11-21 18:22:56 +0200

    Add node project root content

---
+++ build/precache-manifest.afd1efd87701119ec58a2e0fe8d13399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a471422baf3c778f2fc3a08a87e28dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a67e96a66c7403640ba1",
+    "url": "/static/css/2.8f325dac.chunk.css"
+  },
+  {
+    "revision": "604a17cf875b7d931399",
+    "url": "/static/css/main.eebc6973.chunk.css"
+  },
+  {
+    "revision": "a67e96a66c7403640ba1",
+    "url": "/static/js/2.dd642c96.chunk.js"
+  },
+  {
+    "revision": "44a6413f16fd3d2ab5fdc80120c8d103",
+    "url": "/static/js/2.dd642c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604a17cf875b7d931399",
+    "url": "/static/js/main.f6fa9f20.chunk.js"
+  },
+  {
+    "revision": "2b4d8073df71b39be47a",
+    "url": "/static/js/runtime-main.6d871f28.js"
+  }
+]);
